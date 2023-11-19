5a8b5c50f139749e53f736218f5e18a55bade61f
mjamecny <jamecny.m@tutanota.com>
2023-11-19 18:57:50 +0100

    add router and create routes for some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+import AppLayout from "./ui/AppLayout"
+import Countries from "./pages/Countries"
+import PageNotFound from "./pages/PageNotFound"
+
 export default function App() {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="countries" />} />
+          <Route path="countries" element={<Countries />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
