9849f727746286d90ff1f42423d483d6f30f45f2
mjamecny <jamecny.m@tutanota.com>
2023-11-19 19:24:44 +0100

    add structure and styles to AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,3 +1,14 @@
+import { Outlet } from "react-router-dom"
+
+import Header from "./Header"
+
 export default function AppLayout() {
-  return <div>AppLayout</div>
+  return (
+    <div className="grid grid-cols-1 grid-rows-[80px_1fr]">
+      <Header />
+      <main className="px-1.6 bg-veryLightGray max-w-[1200px]">
+        <Outlet />
+      </main>
+    </div>
+  )
 }
