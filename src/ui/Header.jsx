2989acdb1e7dd82ef2bee5341090877ae7678f37
mjamecny <jamecny.m@tutanota.com>
2023-11-19 19:25:03 +0100

    add structure and style to Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,3 +1,8 @@
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div className="flex justify-between items-center bg-white p-1.6 shadow-lg">
+      <h1 className="font-extrabold">Where in the world?</h1>
+      <p>Dark Mode</p>
+    </div>
+  )
 }
