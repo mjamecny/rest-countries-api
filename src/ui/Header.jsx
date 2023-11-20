61148d2c7d91d196908e075d179a10f43115a521
mjamecny <jamecny.m@tutanota.com>
2023-11-20 15:22:27 +0100

    add button for dark mode and change some styles

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,23 @@
+import { CiDark } from "react-icons/ci"
+
+import { useDarkMode } from "../context/DarkModeContext"
+
 export default function Header() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
+
   return (
-    <div className="flex justify-between items-center bg-white p-1.6 shadow-lg">
-      <h1 className="font-extrabold">Where in the world?</h1>
-      <p>Dark Mode</p>
+    <div className=" bg-white dark:bg-darkBlue shadow-lg">
+      <div className="max-w-[1200px] mx-auto flex justify-between items-center px-1.6 py-2.4">
+        <h1 className="font-extrabold dark:text-white lg:text-2.4">
+          Where in the world?
+        </h1>
+        <button
+          className="flex items-center gap-0.8 dark:text-white"
+          onClick={toggleDarkMode}
+        >
+          <CiDark /> Dark Mode
+        </button>
+      </div>
     </div>
   )
 }
