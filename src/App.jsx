1d5e38f8bd1f0fe33074f1c0f9d08a9a760c7f0f
mjamecny <jamecny.m@tutanota.com>
2023-11-20 14:20:43 +0100

    create dark mode context and import to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
+import { DarkModeProvider } from "./context/DarkModeContext"
+
 import AppLayout from "./ui/AppLayout"
 import Countries from "./pages/Countries"
 import PageNotFound from "./pages/PageNotFound"
+import Country from "./pages/Country"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="countries" />} />
-          <Route path="countries" element={<Countries />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <DarkModeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="countries" />} />
+            <Route path="countries" element={<Countries />} />
+            <Route path="countries/:name" element={<Country />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </DarkModeProvider>
   )
 }
